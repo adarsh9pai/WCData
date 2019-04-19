5c8d0b6e9a46505529c349b197b64d98931956f9
Adarsh Pai <adarsh9pai@gmail.com>
2019-04-19 09:47:50 -0500

    Both requirements done on backend.

--- server/api.js
+++ server/api.js
@@ -17,8 +17,25 @@ getStartingLineup = (country, callback) => {
         else{
             connection.query(sqlStatement, (error, results, fields) => {
                 callback(false, results);
-                return results;
             })
+            connection.release()
+        }
+    })
+}
+
+getCardHistory = (parameters, callback) => {
+    let sqlStatement = "SELECT GAME.GameID, GAME.Team1_Score, GAME.Team2_Score, PLAYER.FIFA_Popular_Name, CARDS.Color_Card FROM GAME INNER JOIN TEAM ON (TEAM.TeamID = GAME.TeamID1 OR TEAM.TeamID = GAME.TeamID2) AND TEAM.Team = '" + parameters[0] + "' INNER JOIN CARDS ON (CARDS.GameID = GAME.GameID AND CARDS.TeamID = TEAM.TeamID) INNER JOIN PLAYER ON (PLAYER.PlayerID= CARDS.PlayerID AND PLAYER.TeamID = TEAM.TeamID) WHERE CARDS.Color_Card = '" + parameters[1] + "' ORDER BY GAME.GameID;";
+    pool.getConnection((error, connection) => {
+        if(error){ 
+            console.log(err);
+            callback(true); 
+            return;
+        }
+        else{
+            connection.query(sqlStatement, (error, results, fields) => {
+                callback(false, results);
+            })
+            connection.release()
         }
     })
 }
@@ -30,6 +47,13 @@ api.get('/',(request,response)=>{
 api.get('/startingLineup',(request, response)=>{
     getStartingLineup(request.query.team, (error, returnValue) => {
         response.json({message : "The Starting Lineup for " + request.query.team,
+values : returnValue});
+    })
+})
+
+api.get('/cardHistory',(request, response)=>{
+    getCardHistory([request.query.team,request.query.color], (error, returnValue) => {
+        response.json({message : request.query.color + " Card History for Team " + request.query.team,
 values : returnValue});
     })
 })
