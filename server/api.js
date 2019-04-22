3deb33f0db1e6000d7fdb10046015f9851494eb3
Adarsh Pai <adarsh9pai@gmail.com>
2019-04-22 13:48:13 -0500

    final submission version

--- server/api.js
+++ server/api.js
@@ -6,29 +6,35 @@ let pool = mysql.createPool(connectionCredentials);
 
 let returnValue = '';
 
+
 getStartingLineup = (country, callback) => {
     let sqlStatement = "SELECT GAME.GameID, STARTING_LINEUPS.PlayerID, PLAYER.FIFA_Popular_Name FROM GAME INNER JOIN TEAM ON (TEAM.TeamID = GAME.TeamID1 OR TEAM.TeamID = GAME.TeamID2) AND TEAM.Team = '" + country +"' INNER JOIN STARTING_LINEUPS ON (STARTING_LINEUPS.GameID = GAME.GameID AND STARTING_LINEUPS.TeamID = TEAM.TeamID) INNER JOIN PLAYER ON STARTING_LINEUPS.PlayerID = PLAYER.PlayerID AND PLAYER.TeamID = TEAM.TeamID ORDER BY GAME.GameID, PLAYER.PlayerID;";
     pool.getConnection((error, connection) => {
-        if(error){ 
+        if(error){
             console.log(err);
-            callback(true); 
+            callback(true);
             return;
+
         }
         else{
             connection.query(sqlStatement, (error, results, fields) => {
                 callback(false, results);
             })
             connection.release()
+
+
         }
+
+
     })
 }
 
 getCardHistory = (parameters, callback) => {
     let sqlStatement = "SELECT GAME.GameID, GAME.Team1_Score, GAME.Team2_Score, PLAYER.FIFA_Popular_Name, CARDS.Color_Card FROM GAME INNER JOIN TEAM ON (TEAM.TeamID = GAME.TeamID1 OR TEAM.TeamID = GAME.TeamID2) AND TEAM.Team = '" + parameters[0] + "' INNER JOIN CARDS ON (CARDS.GameID = GAME.GameID AND CARDS.TeamID = TEAM.TeamID) INNER JOIN PLAYER ON (PLAYER.PlayerID= CARDS.PlayerID AND PLAYER.TeamID = TEAM.TeamID) WHERE CARDS.Color_Card = '" + parameters[1] + "' ORDER BY GAME.GameID;";
     pool.getConnection((error, connection) => {
-        if(error){ 
+        if(error){
             console.log(err);
-            callback(true); 
+            callback(true);
             return;
         }
         else{
@@ -41,7 +47,7 @@ getCardHistory = (parameters, callback) => {
 }
 
 api.get('/',(request,response)=>{
-    response.json({message : "WCData API Endpoint"});      
+    response.json({message : "WCData API Endpoint"});
 });
 
 api.get('/startingLineup',(request, response)=>{
@@ -49,6 +55,7 @@ api.get('/startingLineup',(request, response)=>{
         response.json({message : "The Starting Lineup for " + request.query.team,
 values : returnValue});
     })
+
 })
 
 api.get('/cardHistory',(request, response)=>{
@@ -56,6 +63,7 @@ api.get('/cardHistory',(request, response)=>{
         response.json({message : request.query.color + " Card History for Team " + request.query.team,
 values : returnValue});
     })
+
 })
 
 module.exports = api;
